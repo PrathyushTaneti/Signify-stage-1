97b2c424d5e4ece4495b96044b89fa3940da32aa
Prathyush Taneti <prathyushtaneti.11@gmail.com>
2023-03-05 20:15:49 +0530

    New Changes

--- script-files/module-one.js
+++ script-files/module-one.js
@@ -2,7 +2,7 @@ function validate() {
   var username = document.getElementById("name").value;
   var password = document.getElementById("pass").value;
   if (username == "admin" && password == "admin") {
-    window.open("Module2.html");
+    window.open("./Module2.html");
   } else {
     alert("Access Denied");
     return false;
